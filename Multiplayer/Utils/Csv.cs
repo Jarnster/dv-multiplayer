5f8e7305f43a067147fe8ab1f64c872d9d3f4050
AMacro <andrew@andrewcraigmackenzie.com>
2024-07-14 13:56:32 +1000

    Add CSV parsing check and log

--- Multiplayer/Utils/Csv.cs
+++ Multiplayer/Utils/Csv.cs
@@ -40,6 +40,13 @@ namespace Multiplayer.Utils
                     continue;
 
                 string rowKey = values[0];
+              
+                //ensure we don't have too many columns
+                if (values.Count > columns.Count)
+                {
+                    Multiplayer.LogWarning($"CSV Line {i + 1}: Found {values.Count} columns, expected {columns.Count}\r\n\t{line}");
+                    continue;
+                }
 
                 // Add the row values to the appropriate column dictionaries
                 for (int j = 0; j < values.Count && j < keys.Count; j++)
